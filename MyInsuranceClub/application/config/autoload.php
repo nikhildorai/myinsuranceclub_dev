344bb1b0c15b45510d3ea50f69842e98cfe5f0a1
nikhildorai <nikhildorai@gmail.com>
2014-06-25 18:40:31 +0530

    uc

--- MyInsuranceClub/application/config/autoload.php
+++ MyInsuranceClub/application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('database', 'session', 'Template', 'util', 'disqusLib', 'curl');
+$autoload['libraries'] = array('database', 'session', 'Template', 'util', 'curl');
 
 
 /*
