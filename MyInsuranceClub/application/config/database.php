e224b8916505edff3599f1e2b252a5b509b2f99c
nikhildorai <nikhildorai@gmail.com>
2014-07-10 17:37:48 +0530

    updating db file

--- MyInsuranceClub/application/config/database.php
+++ MyInsuranceClub/application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';/* database settings can be created for production,dev
 $active_record = TRUE;
 
 //localhost
-$db['default']['hostname'] = '192.168.2.201';
-$db['default']['username'] = 'developer';
-$db['default']['password'] = 'developer';
+$db['default']['hostname'] = 'localhost';
+$db['default']['username'] = 'root';
+$db['default']['password'] = 'root';
 $db['default']['database'] = 'myinsuranceclubDB';
 
 /*
