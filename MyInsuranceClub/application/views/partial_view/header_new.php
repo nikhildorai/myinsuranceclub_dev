3df0212927d5482a51700bd09daae14858cb509c
rammyinsurance <rameswar.rath@gmail.com>
2014-07-17 11:49:34 +0530

    ram font awesome changes

--- MyInsuranceClub/application/views/partial_view/header_new.php
+++ MyInsuranceClub/application/views/partial_view/header_new.php
@@ -15,7 +15,7 @@
 
 <!-- Bootstrap third-party plugins css -->
 <!-- Font Awesome -->
-<link href="<?php echo base_url();?>/assets/css/font-awesome.min.css" rel="stylesheet">
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 
 <!-- Plugins -->
 
