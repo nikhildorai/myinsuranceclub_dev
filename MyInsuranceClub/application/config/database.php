2f3162496a6410cd8d35aaa073aa7a911bdc79d3
rammyinsurance <rameswar.rath@gmail.com>
2014-07-21 10:46:18 +0530

    data base config

--- MyInsuranceClub/application/config/database.php
+++ MyInsuranceClub/application/config/database.php
@@ -47,19 +47,19 @@
 
 $active_group = 'default';/* database settings can be created for production,development and testing environment.*/
 $active_record = TRUE;
-/*
+
 //localhost
 $db['default']['hostname'] = '192.168.2.201';
 $db['default']['username'] = 'root';
 $db['default']['password'] = 'root';
 $db['default']['database'] = 'myinsuranceclubDB';
-*/
 
-//	internal server
-$db['default']['hostname'] = '192.168.2.201';
-$db['default']['username'] = 'developer';
-$db['default']['password'] = 'developer';
-$db['default']['database'] = 'myinsuranceclubDB';
+
+// //	internal server
+// $db['default']['hostname'] = '192.168.2.201';
+// $db['default']['username'] = 'developer';
+// $db['default']['password'] = 'developer';
+// $db['default']['database'] = 'myinsuranceclubDB';
 
 
 $db['default']['dbdriver'] = 'mysqli';
