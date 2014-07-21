3edf2c1ed0eaad2df343d38119aefdc46423945a
rammyinsurance <rameswar.rath@gmail.com>
2014-07-21 14:43:09 +0530

    company detail

--- MyInsuranceClub/application/views/company_page/companyDetail.php
+++ MyInsuranceClub/application/views/company_page/companyDetail.php
@@ -1,1 +1,379 @@
-<h1>company Detail page</h1>
+<link rel="stylesheet" href="<?php echo base_url();?>assets/css/company.css">
+<div id="highlighted" style="background:#fff; padding-bottom:50px; margin-bottom:0px;" >
+  <div class="container">
+    <div class="row">
+      <div class="col-md-9" style="padding-right:0px;">
+        <h2 class="title-divider"> <span>Canara HSBC Oriental Bank of Commerce Life Insurance Company</span> </h2>
+        <div class="top_col border-box-1 radius2" >
+          <ul class="listsocial">
+            <li class="google">
+              <script type="text/javascript">
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/plusone.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+</script>
+              <g:plusone size="medium"></g:plusone>
+            </li>
+            <li class="like" style="width:79px;height:10px;"><span id="ctl00_cphBody_lblfacebook">
+              <iframe src='//www.facebook.com/plugins/like.php?href=http://www.myinsuranceclub.com/life-insurance/companies/canara-hsbc&amp;send=false&amp;layout=button_count&amp;width=450&amp;show_faces=false&amp;font&amp;colorscheme=light&amp;action=like&amp;height=65' scrolling='no' frameborder='0' style='border:none; overflow:hidden; width:450px; height:65px;' allowTransparency='true'></iframe>
+              </span></li>
+            <li class="reweet"><span id="ctl00_cphBody_lbltweet"><a href='http://twitter.com/share' class='twitter-share-button' data-url='http://www.myinsuranceclub.com/life-insurance/companies/canara-hsbc' data-count='horizontal'>Tweet</a>
+              <script type='text/javascript' src='http://platform.twitter.com/widgets.js'></script>
+              </span></li>
+            <li class="last"><span id="ctl00_cphBody_ViewCount_lblViewsCount"> 17,083</span>&nbsp;Views</li>
+          </ul>
+        </div>
+        <div class="company_term">
+          <h3 class="t_cmp_header"> <span>List of Plans from Canara HSBC OBC</span> </h3>
+          <div class="col-md-6 fst">
+            <h3 class="sub_h_cmp">Term Insurance Plan</h3>
+            <ul class="cmp_ul">
+              <li> <a href="javascript:void(0)">Canara HSBC eSmart Term Plan Online </a> </li>
+              <li class="cmp_link"><a href="javascript:void(0)">Compare Term Insurance Plans <i class="fa fa-arrow-right"></i></a></li>
+            </ul>
+            <h3 class="sub_h_cmp">Child Plans</h3>
+            <ul class="cmp_ul">
+              <li> <a href="javascript:void(0)"> Canara HSBC Future Smart Plan</a></li>
+              <li> <a href="javascript:void(0)">Shubh Laabh</a></li>
+              <li class="cmp_link"><a href="javascript:void(0)">Compare Child Plans <i class="fa fa-arrow-right"></i></a></li>
+            </ul>
+            <h3 class="sub_h_cmp">Unit Linked Insurance Plans - ULIPs</h3>
+            <ul class="cmp_ul">
+              <li> <a href="javascript:void(0)"> Canara HSBC Grow Smart Plan</a></li>
+              <li> <a href="javascript:void(0)">Canara HSBC Dream Smart Plan</a></li>
+              <li> <a href="javascript:void(0)">Canara HSBC Insure Smart Plan</a></li>
+              <li class="cmp_link"><a href="javascript:void(0)">Compare ULIPs <i class="fa fa-arrow-right"></i></a></li>
+            </ul>
+              <ul class="cmp_ul" style="margin-top:30px;">
+                      <li> View <a href="javascript:void(0)">List of Withdrawn Plans from Canara HSBC OBC</a></li>
+              </ul>
+          </div>
+          <div class="col-md-6">
+            <h3 class="sub_h_cmp">Money Back Plan</h3>
+            <ul class="cmp_ul">
+              <li> <a href="javascript:void(0)">Smart Stage Money Back Plan</a> </li>
+              <li class="cmp_link"><a href="javascript:void(0)">Compare Money Back Plans <i class="fa fa-arrow-right"></i></a></li>
+            </ul>
+            <h3 class="sub_h_cmp">Monthly Income Plan</h3>
+            <ul class="cmp_ul">
+              <li> <a href="javascript:void(0)">Canara HSBC Smart Monthly Income Plan</a></li>
+            </ul>
+            <h3 class="sub_h_cmp">Annuity Plan</h3>
+            <ul class="cmp_ul">
+              <li> <a href="javascript:void(0)"> Immediate Pension</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="company_abt">
+          <h3 class="t_cmp_header"> <span>About Canara HSBC OBC</span> </h3>
+          <p class="mar-20"> <img src="assets/images/logo/canara_hsbc_obc_life_logo_172x68.jpg" align="left" s class="cus_border_im"> <strong>Canara HSBC Oriental Bank of Commerce Life Insurance Company</strong> is a joint venture between <strong>Canara Bank</strong>, <strong>Oriental Bank of Commerce</strong> and <strong>HSBC Insurance</strong>. The company started operations in India in 2008. Collectively the company has a network of 4,400 bank branches.<br>
+            <br>
+            <strong>Canara Bank</strong> is owned by the Government of India and has a century old history of banking in India. It is one of the largest banks in India with a amazing network of more than 2800 branches across the length and breadth of India. The <strong>HSBC Group</strong>, headquartered in London, is one of the world's largest banking and financial services organizations with business in more than 81 countries across the world. The HSBC group's presence in India also dates back to more than a century. HSBC Bank is one of the oldest and leading private sector banks in India. <strong>Oriental Bank of Commerce</strong> (OBC), is also a Government of India's undertaking with a network of more than 1526 branches making it among the larger banks in India. Overall this partnership has got a trustworthy and historically established background to run the insurance business.<br>
+            <br>
+            The company in its current form operated on a pure Bancassurance model. Having a large customer base of 3 banks, is in itself a huge base to start selling insurance products.</p>
+        </div>
+        <div class="company_abt">
+          <h3 class="t_cmp_header"> <span>Leadership</span> </h3>
+          <p class="mar-20" align="center" style="width:100%; float:left; text-align:center;">
+          
+          <img src="assets/images/company_page/john_holden_canara_hsbc_ceo.jpg" class="round_image" >
+          
+          </p>
+         <p align="center" style="width:100%; float:left; text-align:center;"> <strong>John Holden&nbsp;</strong>is the<strong>&nbsp;</strong>CEO of<strong>&nbsp;Canara HSBC Life Insurance Company.</strong></p>
+        </div>
+        <div class="company_abt">
+          <h3 class="t_cmp_header"> <span>Contact Details</span> </h3>
+          
+          
+          
+          <div class="col-md-6 no_pad_l mar-20">
+          
+          
+          <address class="adr span6">
+           <em class="fa fa-map-marker"></em><strong class="t_h">Corporate Office</strong><br>
+<div class="road">Canara HSBC Life Insurance,<br>
+                  1st Floor, Tower B, First India Place,<br>
+                  Vatika Tower, Sushant Lok Phase 1,<br>
+                  Mehrauli-Gurgaon Road,<br>
+                  Gurgaon 122002,<br>
+                  Haryana</div> <br>
+
+
+</address>
+<div style="float:left; width:100%; margin-top:20px;">
+
+  <span class="follow"><em class="fa fa-twitter"></em></span>
+
+<span class="follow"><em class="fa fa-facebook"></em></span>
+
+<span class="follow"><em class="fa fa-google-plus"></em></span>
+</div>
+         
+                 
+                  
+          </div>
+          
+          
+          <div class="col-md-6 mar-20 no_pad_l">
+           <span class="phone"><em class="fa fa-phone"></em>1800 103 0003</span><br>
+           <span class="web"><em class="fa fa-globe"></em>www.canarahsbclife.com</span><br>
+                  
+                  <span class="mail"> <em class="fa fa-envelope"></em>customerservice@canarahsbclife.in</span><br>
+                  
+                  <span class="phone"><em class="fa fa-envelope-o"></em>97790 30003</span>
+                 
+
+          </div>
+          
+          
+          
+        </div>
+        
+        
+        
+        
+        
+        
+         <div class="company_abt claim_r">
+          <h3 class="t_cmp_header"> <span>Claims ratio of Canara HSBC OBC</span> </h3>
+          
+          
+          <table class="mar-20">
+  <thead>
+    <tr>
+      <th>Years</th>
+      <th>2005 - 06</th>
+     <th>2006 - 07</th>
+     <th>2007 - 08</th>
+     <th>2008 - 09</th>
+     <th>2009 - 10</th>
+     <th>2010 - 11</th>
+     <th>2011 - 12</th>
+     <th>2012 - 13</th>
+     <th>2013 - 14</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Claims ratio</td>
+        <td>92%</td>
+        <td>93%</td>
+        <td>94%</td>
+        <td>95%</td>
+        <td>97%</td>
+        <td>94%</td>
+        <td>99%</td>
+        <td>98%</td>
+        <td>102%</td>
+     
+    </tr>
+   
+  </tbody>
+</table>
+<div class="col-md-12 text-rightp" style="margin-top:15px;"><a href="javascript:void(0)">View Older Data <span class="ic">+</span></a></div>
+
+        </div>
+        
+        
+        
+        
+        
+        
+        <div class="company_abt claim_r">
+          <h3 class="t_cmp_header"> <span>NAVs of funds in ULIPs from Canara HSBC OBC</span> </h3>
+          
+          
+          <table class="mar-20">
+  <thead>
+    <tr>
+      <th>ULIPs</th>
+      <th>NAVs (in Rs.)</th>
+     <th>Date</th>
+     <th>Change</th>
+     <th>% Change</th>
+    
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>AEGON Religare Assure Plus Plan - Accelerator Fund </td>
+        <td>16.75400</td>
+        <td>16-05-2014</td>
+        <td>0.13</td>
+        <td>0.78</td>
+     
+    </tr>
+     <tr>
+      <td>AEGON Religare Assure Plus Plan - Debt Fund </td>
+        <td>17.75400</td>
+        <td>16-05-2014</td>
+        <td>0.11</td>
+        <td>0.98</td>
+     
+    </tr>
+    
+     <tr>
+      <td>AEGON Religare Assure Plus Plan - Secure Fund </td>
+        <td>16.75400</td>
+        <td>16-05-2014</td>
+        <td>0.13</td>
+        <td>0.78</td>
+     
+    </tr>
+    
+     <tr>
+      <td>AEGON Religare Assure Plus Plan - Accelerator Fund </td>
+        <td>15.75400</td>
+        <td>16-05-2014</td>
+        <td>0.03</td>
+        <td>0.98</td>
+     
+    </tr>
+    
+    
+     <tr>
+      <td>AEGON Religare Assure Plus Plan - Discontinued Policy Fund </td>
+        <td>18.75400</td>
+        <td>16-05-2014</td>
+        <td>0.10</td>
+        <td>0.03</td>
+     
+    </tr>
+   
+  </tbody>
+</table>
+<div class="col-md-12 text-rightp" style="margin-top:15px;"><a href="javascript:void(0)">View Older Data <span class="ic">+</span></a></div>
+
+
+        </div>
+        
+            <!--<div class="company_abt one_time" id="ad_show_premium">
+  <div class="col-md-12 mar-25 no_pad_lr">
+     
+     
+        <div id="claims_ratio1" style="min-width:50%; height: 400px; margin: 0 auto"></div>
+        
+        </div>
+        </div>-->
+        
+        
+        <div class="company_abt ">
+  <div class="col-md-12 mar-25 no_pad_lr">
+    <div class="top_ins"></div>
+    <h3 class="header_art">News on Canara HSBC OBC</h3>
+     <div class="col-md-6" style="padding-left:0px;">
+    <div class="art_cnt widget ">
+      <h4 class="sub_h" style="margin-top:0px;">How to secure your future with pension</h4>
+      <div class="textwidget">
+        <p><img style="border: 0px none;" alt="" src="assets/images/art1.jpg">At any moment, an unhappy customer can share their opinion with the masses through...How to speak with an Indian Accent. </p>
+      </div>
+      <div class="comnt"> <span class="text-left l">1,348 views</span> <span class="text-right r">0 comments</span> </div>
+    </div>
+    <div class="art_cnt widget ">
+      <h4 class="sub_h" style="margin-top:0px;">How to secure your future with pension</h4>
+      <div class="textwidget">
+        <p><img style="border: 0px none;" alt="" src="assets/images/art1.jpg">At any moment, an unhappy customer can share their opinion with the masses through...How to speak with an Indian Accent. </p>
+      </div>
+      <div class="comnt"> <span class="text-left l">1,348 views</span> <span class="text-right r">0 comments</span> </div>
+    </div>
+    </div>
+     <div class="col-md-6 ">
+    <div class="art_cnt widget ">
+      <h4 class="sub_h" style="margin-top:0px;">How to secure your future with pension</h4>
+      <div class="textwidget">
+        <p><img style="border: 0px none;" alt="" src="assets/images/art1.jpg">At any moment, an unhappy customer can share their opinion with the masses through...How to speak with an Indian Accent. </p>
+      </div>
+      <div class="comnt"> <span class="text-left l">1,348 views</span> <span class="text-right r">0 comments</span> </div>
+    </div>
+    <div class="art_cnt widget ">
+      <h4 class="sub_h" style="margin-top:0px;">How to secure your future with pension</h4>
+      <div class="textwidget">
+        <p><img style="border: 0px none;" alt="" src="assets/images/art1.jpg">At any moment, an unhappy customer can share their opinion with the masses through...How to speak with an Indian Accent. </p>
+      </div>
+      <div class="comnt"> <span class="text-left l">1,348 views</span> <span class="text-right r">0 comments</span> </div>
+    </div>
+    </div>
+    <div class="col-md-12 text-rightp"><a href="javascript:void(0)">More Articles <span class="ic">+</span></a></div>
+  </div>
+ 
+  
+    
+    
+
+</div>
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+         <div class="company_abt  ">
+  <div class="col-md-12 mar-25 no_pad_lr">
+    <div class="top_ins"></div>
+    <h3 class="header_art">Videos on Canara HSBC OBC</h3>
+     <div class="col-md-4" style="padding-left:0px;">
+   
+    <div class="art_cnt widget video">
+      <div class="textwidget">
+       
+      <a href="http://www.youtube.com/watch?v=zkdv8Plk58Q" target="_blank"><img src="http://img.youtube.com/vi/zkdv8Plk58Q/0.jpg" class="img_t" style="max-width:94%;" border="0"></a>
+       
+      </div>
+      <div class="comnt"><p>I have been sold 7 ulip policies</p> </div>
+    </div>
+    </div>
+     <div class="col-md-4 ">
+    <div class="art_cnt widget video">
+      <div class="textwidget">
+       
+            <a href="http://www.youtube.com/watch?v=lUxY6sx6U9s" target="_blank"><img src="http://img.youtube.com/vi/lUxY6sx6U9s/0.jpg" class="img_t" style="max-width:100%;" border="0"></a>
+
+        
+      </div>
+      <div class="comnt"><p>I have been sold 7 ulip policies</p> </div>
+    </div>
+    </div>
+    
+    
+    <div class="col-md-4 ">
+    <div class="art_cnt widget video">
+      <div class="textwidget">
+       
+            <a href="http://www.youtube.com/watch?v=kOhdPbf7p3M" target="_blank"><img src="http://img.youtube.com/vi/kOhdPbf7p3M/0.jpg" class="img_t" style="max-width:100%;" border="0"></a>
+
+        
+      </div>
+      <div class="comnt"><p>I have been sold 7 ulip policies</p> </div>
+
+    </div>
+    </div>
+    
+    <div class="col-md-12 text-rightp"><a href="javascript:void(0)">More Videos <span class="ic">+</span></a></div>
+  </div>
+ 
+  
+    
+    
+
+</div>
+        
+        
+        
+        
+        
+      </div>
+      <div class="col-md-3 sidebar sidebar-right">
+        <div class="inner">
+          <div class="block"> </div>
+          <div class="block"> </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
