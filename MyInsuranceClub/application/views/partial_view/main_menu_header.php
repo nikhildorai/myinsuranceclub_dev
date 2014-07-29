b40f0443e00ae8a555da9f2552f8103ba797197e
nikhildorai <nikhildorai@gmail.com>
2014-07-29 18:01:59 +0530

    update link

--- MyInsuranceClub/application/views/partial_view/main_menu_header.php
+++ MyInsuranceClub/application/views/partial_view/main_menu_header.php
@@ -53,7 +53,7 @@
                            <li class="col-md-4" > <a role="menuitem" href="#" tabindex="-1" class="menu-item"><strong>Money Back Plans</strong></a> </li>
                            <li class="col-md-4" > <a role="menuitem" href="#" tabindex="-1" class="menu-item"><strong>Pension Plans</strong></a> </li>
                            <li class="col-md-4" > <a role="menuitem" href="#" tabindex="-1" class="menu-item"><strong>Endowment Plans</strong></a> </li>
-                           <li class="col-md-4" > <a role="menuitem" href="<?php echo site_url('critical-iIllness');?>" tabindex="-1" class="menu-item"><strong>Critical Illness</strong></a> </li>
+                           <li class="col-md-4" > <a role="menuitem" href="<?php echo site_url('critical-illness');?>" tabindex="-1" class="menu-item"><strong>Critical Illness</strong></a> </li>
                            <li class="col-md-4" > <a role="menuitem" href="<?php echo site_url('health_insurance/personal_accident');?>" tabindex="-1" class="menu-item"><strong>Personal Accident</strong></a> </li>
                         </ul>
                      </li>
