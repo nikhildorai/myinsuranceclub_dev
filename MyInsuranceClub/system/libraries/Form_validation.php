49066bdf321eedf663fc6df249eb59ff46e17782
nikhildorai <nikhildorai@gmail.com>
2014-08-01 15:46:28 +0530

    updating form validation library

--- MyInsuranceClub/system/libraries/Form_validation.php
+++ MyInsuranceClub/system/libraries/Form_validation.php
@@ -1144,7 +1144,7 @@ class CI_Form_validation {
 	 */
 	public function age_greater_than_18($str)
 	{
-		$birthDate=explode('-',$str);
+		$birthDate=explode('/',$str);
 		$age = (date("md", date("U", mktime(0, 0, 0, $birthDate[0], $birthDate[1], $birthDate[2]))) > date("md")
 				? ((date("Y") - $birthDate[2]) - 1)
 				: (date("Y") - $birthDate[2]));
